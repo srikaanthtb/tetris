afc8d3c1ebf9b6853ac4da1407adcb4fa364adff
srikaanthtb <srikaanth.t.b@gmail.com>
2020-12-02 20:58:04 +0000

    added game over

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const StartBtn = document.querySelector('#start-button')
     const width = 10
     let nextRandom = 0
+    let score = 0
     let timerId
     const lTetromino = [
         [1, width+1, width*2+1, 2],
@@ -91,6 +92,8 @@ function freeze(){
       currentPosition = 4
       draw()
       displayShape()
+      addScore()
+      gameOver()
   }
 }
 
@@ -163,8 +166,29 @@ StartBtn.addEventListener('click', () => {
     }
 } )
 
+function addScore(){
+    for (let i = 0; i < 199; i +=width){
+        const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
 
+        if(row.every(index => squares[index].classList.contains('taken'))){
+            score += 10
+            ScoreDisplay.innerHTML = score
+            row.forEach(index => {
+                squares[index].classList.remove('taken')
+                squares[index].classList.remove('tetromino')
+            })
+            const squaresRemoved = squares.splice(i, width)
+            squares = squaresRemoved.concat(squares)
+            squares.forEach(cell => grid.appendChild(cell))
+        }
+    }
+}
 
-
+function gameOver() {
+    if(current.some(index => squares[currentPosition+ index].classList.contains('taken'))){
+        ScoreDisplay.innerHTML = 'end'
+        clearInterval(timerId)
+    }
+}
 
 })
