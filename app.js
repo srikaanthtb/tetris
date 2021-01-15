5401184f42b5503c3ce8b689a433b8f58d2d2e7a
srikaanthtb <srikaanth.t.b@gmail.com>
2021-01-15 15:14:08 +0000

    text fixes and start of css changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
+    const PointsDisplay = document.querySelector('#points')
     const ScoreDisplay = document.querySelector('#score')
     const StartBtn = document.querySelector('#start-button')
     const width = 10
@@ -227,7 +228,7 @@ function addScore(){
 
 function gameOver() {
     if(current.some(index => squares[currentPosition+ index].classList.contains('taken'))){
-        ScoreDisplay.innerHTML = 'end'
+        PointsDisplay.innerHTML = 'end'
         clearInterval(timerId)
     }
 }
